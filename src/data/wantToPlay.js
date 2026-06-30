49edf900466a62cff8127353ba4c8405f1bf75c3
Christopher J. Adkins <chris@cjadkins.com>
2026-06-29 22:23:48 -0400

    feat: swap Helldivers for Warcraft 3 and World of Claudecraft

--- src/data/wantToPlay.js
+++ src/data/wantToPlay.js
@@ -1,7 +1,8 @@
 const wantToPlay = [
   { title: "Split Fiction", weight: 1, staple: false },
-  { title: "Helldivers", weight: 1, staple: false },
   { title: "World of Warcraft", weight: 1, staple: false },
+  { title: "Warcraft 3", weight: 1, staple: false },
+  { title: "World of Claudecraft", weight: 1, staple: false },
   { title: "USB CTF", weight: 1, staple: false },
   { title: "Heroes of the Storm", weight: 1, staple: true },
   { title: "Overwatch", weight: 1, staple: true },
